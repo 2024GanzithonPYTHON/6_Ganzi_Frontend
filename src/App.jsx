dc831a4730152980c70fe566891fc10a3abbe43a
PC2408 <applelgy@naver.com>
2024-11-11 17:46:46 +0900

    브랜치 변경 및 커밋확인

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import styled from "styled-components"
+
+const StyledEx = styled.span`
+ background-color: #B86217;
+ color: Yellow;
+ font-size: 50px;
+ font-weight: bold;
+ padding: 30px;
+ `;
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <StyledEx>Flan</StyledEx>
+      <br/><br/>
+      <h1> Flan 프론트엔드용 Vite React입니다</h1>
+      <h2>도유성 이가영 파이팅~!</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
