989bbf721c5238b8bd899a65d5ffd04c385ca7b5
yousung <doyousung@naver.com>
2024-11-19 00:34:44 +0900

    로그인 페이지 css 완

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,9 @@
-
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
-import kakaoLoginBtn from '../../assets/login/kakao_login_medium_wide.png';
-
+import kakaoLoginBtn from '../../assets/login/kakao_button.png';
+import onbordIcon from '../../assets/login/onboardingIcon.png';
+import downIcon from '../../assets/login/Expand_down_double_light.png';
 
 const Login = () => {
     const navigate = useNavigate();
@@ -39,13 +39,19 @@ const Login = () => {
     };
   
     return (
-      <div className="login-container">
-        <h1>로그인</h1>
+      <div className="background">
+        <div className="logo-explain">요즘 가족, 일정 관리 서비스</div>
+        <div className="login-logo">FLAN</div>
+        <img src={onbordIcon} alt="온보딩 페이지 아이콘" className="onboardIcon1"></img>
+        <div className="explain">지금 <span className="bold">FLAN</span>과 함께<br/>
+        <span className="bold">개인 일정</span>이 반영되는 <span className="bold">가족 일정</span> 관리를<br/>
+        경험해 보세요!</div>
+        <img src={downIcon} alt="온보딩 페이지 아이콘" className="onboardIcon2"></img>
         <button onClick={handleKakaoLogin} className="kakao-login-button">
           <img src={kakaoLoginBtn} alt="카카오 로그인 버튼" />
         </button>
       </div>
     );
-  };
-  
-  export default Login;
+};
+
+export default Login;
