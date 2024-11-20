995e84edd25e121d13f3abd424ff0465e63f1463
yousung <doyousung@naver.com>
2024-11-21 00:49:30 +0900

    마이페이지 배지 구현

--- src/constants/badgeMapping.js
+++
@@ -1,9 +0,0 @@
-// badgeMapping.js
-const badgeMapping = {
-    "gold_badge": "/images/gold_badge.png",
-    "silver_badge": "/images/silver_badge.png",
-    "bronze_badge": "/images/bronze_badge.png",
-  };
-  
-  export default badgeMapping;
-  
