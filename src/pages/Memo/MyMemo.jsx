df229a902823bcc3875482aa50955c5f89a8ce3a
PC2408 <applelgy@naver.com>
2024-11-21 20:10:10 +0900

    메모실수 정정

--- src/pages/Memo/MyMemo.jsx
+++ src/pages/Memo/MyMemo.jsx
@@ -4,8 +4,6 @@ import MemoContainer from './Memo';
 import SaveButton from './SaveButton'; 
 import TopBar from './TopBar';
 
-import api from '../../api/api';
-
 const Container = styled.div`
     position: sticky;  
     top: 0px;   // 로고 바로 아래로
@@ -15,36 +13,24 @@ const Container = styled.div`
 const ButtonContainer = styled.div`
     margin-top: 50px; 
 `;
+
 function MyMemo() {
     const [memos, setMemos] = useState([]);
     const [content, setContent] = useState(''); // 메모 내용 관리
     const [title, setTitle] = useState(''); // 메모 제목 관리
-    const accessToken = 'YOUR_ACCESS_TOKEN'; // 여기에 실제 access token을 입력하세요
+    
 
     const addMemo = (newMemo) => {
         setMemos([...memos, newMemo]);
     };
 
-    const handleSave = async () => {
+
+
+    const handleSave = () => {
         if (content && title) {
-            const newMemo = { title, content };
-
-            try {
-                const response = await api.post('/personal/one-word/', newMemo, {
-                    headers: {
-                        'Authorization': `Bearer ${accessToken}`,
-                        'Content-Type': 'application/json'
-                    }
-                });
-
-                // 서버에서 응답받은 데이터를 메모에 추가
-                addMemo(response.data); 
-                setContent('');
-                setTitle('');
-            } catch (error) {
-                console.error("메모 저장 실패:", error);
-                alert("메모 저장에 실패했습니다.");
-            }
+            addMemo({ title, content });
+            setContent('');
+            setTitle('');
         } else {
             alert("제목과 내용을 입력해주세요");
         }
