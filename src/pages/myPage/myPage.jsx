a1fa2c41fcaf32059f31597fe730fd57ba7439a4
PC2408 <applelgy@naver.com>
2024-11-23 14:54:11 +0900

    배포 중

--- src/pages/myPage/myPage.jsx
+++ src/pages/myPage/myPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import apiClient from "../../api/axClient";
-import "./MyPage.css";
+import "./myPage.css";
 import BadgeList from "../../badge/BadgeList";
 import logoutImage from '../../assets/mypage/logout.png';
 import rightWay from '../../assets/mypage/rightAngleBracket.png';
