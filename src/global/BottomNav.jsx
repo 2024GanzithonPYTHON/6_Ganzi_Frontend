1c4ce375d577a71ee541612fee28481863f8973e
PC2408 <applelgy@naver.com>
2024-11-23 15:33:29 +0900

    이미지 경로 지정

--- src/global/BottomNav.jsx
+++ src/global/BottomNav.jsx
@@ -15,25 +15,25 @@ const BottomNav = () => {
             </Link>
 
             <Link to="/FamilyCalendar" className="nav-button">
-                    <img src = "public\assets\Global\BottomNav\fam_cal.svg" alt = "가족 캘린더"/>
+                    <img src = "/assets/Global/BottomNav/fam_cal.svg" alt = "가족 캘린더"/>
                     <br/>
                     가족 캘린더
             </Link>
 
             <Link to="/" className="nav-button">
-                <img src = "public\assets\Global\BottomNav\home.svg" alt = "홈"/>
+                <img src = "/assets/Global/BottomNav/home.svg" alt = "홈"/>
                 <br/>
                 홈
             </Link>
 
             <Link to="/" className="nav-button">
-            <img src="public/assets/Global/BottomNav/register.svg" alt="캘린더등록" />
+            <img src="/assets/Global/BottomNav/register.svg" alt="캘린더등록" />
             <br />
             <div className="smaller-text">가족일정 등록</div> {/* 작은 텍스트 클래스 적용 */}
             </Link>
 
             <Link to="/MyPage" className="nav-button">
-                <img src = "public\assets\Global\BottomNav\profile.svg" alt = "프로필"/>
+                <img src = "/assets/Global/BottomNav/profile.svg" alt = "프로필"/>
                 <br/>
                 내 프로필
             </Link>
